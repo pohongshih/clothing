65ef01202d3d1b7af15c2661ee3d8ffd61195cea
pohongshih <pohongshih@gmail.com>
2024-03-26 21:02:52 +0800

    commit4

---
+++ src/main/java/com/example/demo/UserEntityDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class UserEntityDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String phone;
+    private String line;
+    private String role;
+}
